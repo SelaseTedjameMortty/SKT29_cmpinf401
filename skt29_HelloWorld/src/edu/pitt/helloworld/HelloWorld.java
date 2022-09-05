795e3bc70b7817fea2e571843fb062e6c480f50b
SelaseTedjameMortty <79680709+SelaseTedjameMortty@users.noreply.github.com>
2022-09-05 13:39:37 -0400

    Created Hello World Project

---
+++ skt29_HelloWorld/src/edu/pitt/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package edu.pitt.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO WORLD!");
+
+	}
+
+}
